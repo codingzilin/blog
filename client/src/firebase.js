90cd8173f7aa542180e2b0731e22d7782865eb77
Zilin Song <harry.song518@gmail.com>
2024-11-09 23:36:13 +1030

    feat: added google OAuth

---
+++ client/src/firebase.js
@@ -0,0 +1,35 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "personal-blog-99658.firebaseapp.com",
+//   projectId: "personal-blog-99658",
+//   storageBucket: "personal-blog-99658.firebasestorage.app",
+//   messagingSenderId: "966869451875",
+//   appId: "1:966869451875:web:f81b2cf89b2ae1db276456"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDo0qy_hZNCCpsmNFeULZo-_0GZ5NcwsgY",
+  authDomain: "personal-blog-99658.firebaseapp.com",
+  projectId: "personal-blog-99658",
+  storageBucket: "personal-blog-99658.firebasestorage.app",
+  messagingSenderId: "966869451875",
+  appId: "1:966869451875:web:f81b2cf89b2ae1db276456"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
