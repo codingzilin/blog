5fba4daf7dd196ef58fed6500d8411078218f676
Zilin Song <harry.song518@gmail.com>
2024-12-18 15:32:11 +1030

    feat(backend): add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'; //you can define a different name for the route
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 import updateDB from './migrations/updateDB.js';
 dotenv.config();
@@ -28,6 +29,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
