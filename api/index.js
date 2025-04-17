f0eecfa3f09c3e882cda298f846b9182bb7e9656
Zilin Song <harry.song518@gmail.com>
2025-04-17 15:10:22 +0930

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 import updateDB from './migrations/updateDB.js';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
   console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,6 +33,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
